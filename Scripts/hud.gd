e3955ca26b4521455d8195fa59b115ce07f4722f
cocie228 <140580021+cocie228@users.noreply.github.com>
2024-07-24 12:13:22 +0800

    fix

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 func _displaydata():
 	#Выводит инфу в худ
-	$MarginContainer/HP.text ="HP: " + str(Global.PlayerHP)
+	$MarginContainer/HP.text ="HP: " + str(Global.PlayerHP) + "  "
 
 func _process(_delta):
 	_displaydata()
